871fb2e5da293b917c4122024c8731b29fb9f58a
Swagat <swagatmitrabhattacharya18@gmail.com>
2023-10-04 07:00:13 +0530

    initial commit

---
+++ day1.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+struct Elf {
+    index: usize,
+    max_cal: u32,
+}
+
+fn main() {
+    let mut result = Elf {
+        index: 0,
+        max_cal: 0,
+    };
+    let input = fs::read_to_string("./day1.txt").unwrap();
+    let calorie_array: Vec<String> = input.split("\n\n").map(|x| x.to_string()).collect();
+    for (idx, calorie_bunch) in calorie_array.iter().enumerate() {
+        let mut count = 0;
+        let calories: Vec<u32> = calorie_bunch
+            .split("\n")
+            .filter(|x| !x.trim().is_empty()) 
+            .map(|x| x.parse().unwrap())
+            .collect();
+        for calorie in calories {
+            count += calorie;
+        }
+        if count > result.max_cal {
+            result.max_cal = count;
+            result.index = idx + 1
+        }
+    }
+    println!("{} {}", result.max_cal, result.index);
+}
---
+++ day2.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+fn get_result(a: &str, b: &str) -> (u32, u32) {
+    match (a, b) {
+        ("A", "X") => (3, 1),
+        ("A", "Y") => (6, 2),
+        ("A", "Z") => (0, 3),
+        ("B", "X") => (0, 1),
+        ("B", "Y") => (3, 2),
+        ("B", "Z") => (6, 3),
+        ("C", "X") => (6, 1),
+        ("C", "Y") => (0, 2),
+        ("C", "Z") => (3, 3),
+        _ => (0, 0), 
+    }
+}
+
+fn main() {
+    let input = fs::read_to_string("./day2.txt").expect("Could not read file");
+    let rounds: Vec<&str> = input.split("\n").collect();
+    let mut score = 0;
+
+    for round in rounds.iter() {
+        let moves: Vec<&str> = round.split_whitespace().collect();
+        let (result_a, result_b) = get_result(moves[0], moves[1]);
+        score += result_a + result_b
+    }
+
+    println!("score: {}", score);
+}
