b9d19283f5173f61890c9121680bdad2dffa3259
Swagat <swagatmitrabhattacharya18@gmail.com>
2023-10-05 21:04:23 +0530

    commit

--- 22/day8/day8.rs
+++ 22/day8/day8.rs
@@ -1,59 +1,76 @@
-use std::fs; 
+use std::fs;
 
 fn from_left(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> Option<&str> {
-    for height in &trees[row][0..i] {
+    for (idx, height) in (&trees[row][0..i]).iter().enumerate() {
         if height >= &trees[row][i] {
-            return None
+            println!("{}", idx);
+            return None;
         }
+        println!("{}", idx);
     }
     Some("left")
 }
 
 fn from_right(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> Option<&str> {
-    for height in &trees[row][i+1..5] {
+    let length: usize = trees[row].len();
+    for height in &trees[row][i + 1..length] {
         if height >= &trees[row][i] {
-            return None
+            return None;
         }
     }
     Some("right")
 }
 
 fn from_top(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> Option<&str> {
-   for col in 0..row {
-       if &trees[col][i] >= &trees[row][i] {
-         return None
-       }
-   }
-   Some("top")
+    for col in 0..row {
+        if &trees[col][i] >= &trees[row][i] {
+            return None;
+        }
+    }
+    Some("top")
 }
 
 fn from_bottom(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> Option<&str> {
-    for col in (row + 1)..trees.len() { 
+    for col in (row + 1)..trees.len() {
         if &trees[col][i] >= &trees[row][i] {
-            return None
+            return None;
         }
     }
     Some("bottom")
 }
 
-
 fn main() {
-    let input = fs::read_to_string("./day8.txt").unwrap();
-    let tree_arr: Vec<Vec<u32>> = input
-                     .lines()
-                     .map(|x| x.split(",").map(|x| x.parse::<u32>().unwrap()).collect())
-                     .collect();
-    let rows = [1, 2, 3];
+    let mut visible_trees = 0;
+    let input = fs::read_to_string("./day8.txt").expect("Failed to read file");
+    let mut tree_arr: Vec<Vec<u32>> = Vec::new();
+    for line in input.lines() {
+        let mut row: Vec<u32> = Vec::new();
+        for character in line.chars() {
+            if let Some(number) = character.to_digit(10) {
+                row.push(number);
+            }
+        }
+        tree_arr.push(row);
+    }
+    let rows = 1..tree_arr.len() - 1;
     for row in rows {
-        for i in 1..4 {
+        for i in 1..tree_arr[row].len() - 1 {
             let left = from_left(&tree_arr, row, i);
             let right = from_right(&tree_arr, row, i);
             let top = from_top(&tree_arr, row, i);
             let bottom = from_bottom(&tree_arr, row, i);
-            let any_some = [left, right, top, bottom];
-            let unwrapped: Vec<&str> = any_some.iter().filter_map(|x| x.clone()).collect();
-            println!("{} {:?}", tree_arr[row][i], unwrapped);            
+            let unwrapped: Vec<&str> = [left, right, top, bottom]
+                .iter()
+                .filter_map(|x| x.clone())
+                .collect();
+            if !unwrapped.is_empty() {
+                visible_trees += 1;
+            }
+            println!("{} {:?}", tree_arr[row][i], unwrapped);
         }
-    }                          
+    }
+    let rows = tree_arr.len();
+    let cols = tree_arr[0].len();
+    let edge_visible = (rows * cols) - ((rows - 2) * (cols - 2));
+    println!("{}", visible_trees + edge_visible);
 }
-
