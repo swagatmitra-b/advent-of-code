441fd2d633a56e16a3277db3dccc3467a1f30872
Swagat <swagatmitrabhattacharya18@gmail.com>
2023-10-06 00:02:47 +0530

    commit

--- 22/day8/day8.rs
+++ 22/day8/day8.rs
@@ -1,46 +1,49 @@
 use std::fs;
 
-fn from_left(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> Option<&str> {
-    for (idx, height) in (&trees[row][0..i]).iter().enumerate() {
-        if height >= &trees[row][i] {
-            println!("{}", idx);
-            return None;
+fn from_left(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> u32 {
+    for height in (0..i).rev() {
+        if &trees[row][height] >= &trees[row][i] {
+            let value = i - height;
+            return value as u32;
         }
-        println!("{}", idx);
     }
-    Some("left")
+    i as u32
 }
 
-fn from_right(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> Option<&str> {
+fn from_right(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> u32 {
     let length: usize = trees[row].len();
-    for height in &trees[row][i + 1..length] {
-        if height >= &trees[row][i] {
-            return None;
+    for height in i + 1..length {
+        if &trees[row][height] >= &trees[row][i] {
+            let value = height - i;
+            return value as u32;
         }
     }
-    Some("right")
+    (length-(i+1)) as u32
 }
 
-fn from_top(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> Option<&str> {
-    for col in 0..row {
+fn from_top(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> u32 {
+    for col in (0..row).rev() {
         if &trees[col][i] >= &trees[row][i] {
-            return None;
+            let value = row - col;
+            return value as u32;
         }
     }
-    Some("top")
+    row as u32
 }
 
-fn from_bottom(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> Option<&str> {
+fn from_bottom(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> u32 {
     for col in (row + 1)..trees.len() {
         if &trees[col][i] >= &trees[row][i] {
-            return None;
+            let value = col - row;
+            return value as u32;
         }
     }
-    Some("bottom")
+    (trees.len()-1 - row) as u32
 }
 
 fn main() {
-    let mut visible_trees = 0;
+    let mut scenic_score = 0;
+    // let mut visible_trees = 0;
     let input = fs::read_to_string("./day8.txt").expect("Failed to read file");
     let mut tree_arr: Vec<Vec<u32>> = Vec::new();
     for line in input.lines() {
@@ -55,22 +58,38 @@ fn main() {
     let rows = 1..tree_arr.len() - 1;
     for row in rows {
         for i in 1..tree_arr[row].len() - 1 {
+            let mut product = 1;
             let left = from_left(&tree_arr, row, i);
             let right = from_right(&tree_arr, row, i);
             let top = from_top(&tree_arr, row, i);
             let bottom = from_bottom(&tree_arr, row, i);
-            let unwrapped: Vec<&str> = [left, right, top, bottom]
-                .iter()
-                .filter_map(|x| x.clone())
-                .collect();
-            if !unwrapped.is_empty() {
-                visible_trees += 1;
+            let unwrapped = [left, right, top, bottom];
+            for mul in unwrapped {
+                product *= mul;
             }
-            println!("{} {:?}", tree_arr[row][i], unwrapped);
+            if product > scenic_score {
+                scenic_score = product
+            }
+            //     .iter()
+            //     .filter_map(|x| x.clone())
+            //     .collect();
+            // if !unwrapped.is_empty() {
+            //     visible_trees += 1;
+            // }
         }
     }
-    let rows = tree_arr.len();
-    let cols = tree_arr[0].len();
-    let edge_visible = (rows * cols) - ((rows - 2) * (cols - 2));
-    println!("{}", visible_trees + edge_visible);
+    println!("{:?}",scenic_score);
+    // let rows = tree_arr.len();
+    // let cols = tree_arr[0].len();
+    // let edge_visible = (rows * cols) - ((rows - 2) * (cols - 2));
+    // println!("{}", visible_trees + edge_visible);
 }
+
+// fn from_bottom(trees: &Vec<Vec<u32>>, row: usize, i: usize) -> Option<&str> {
+//     for col in (row + 1)..trees.len() {
+//         if &trees[col][i] >= &trees[row][i] {
+//             return None
+//         }
+//     }
+//     Some("bottom")
+// }
