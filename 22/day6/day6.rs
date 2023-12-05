a16ce4c2f3bc0c347e7f39c82abd3ebfa6bdb8ff
Swagat <swagatmitrabhattacharya18@gmail.com>
2023-12-05 15:06:28 +0530

    exec

--- 22/day6/day6.rs
+++ 22/day6/day6.rs
@@ -6,18 +6,17 @@ fn main() {
     let input = fs::read_to_string("./day6.txt").unwrap();
     let mut last_idx = 0;
     for (idx, character) in input.chars().enumerate() {
+        if idx - last_idx >= 13 {
+            println!("{}", idx + 1);
+            return;
+        }
         if let Some(value) = hashmap.get(&character) {
             if *value >= last_idx {
                 last_idx = *value + 1;
-            } 
+            }
             hashmap.insert(character, idx);
         } else {
             hashmap.insert(character, idx);
-            if idx - last_idx >= 4 {
-                println!("{}", idx);
-                return
-            }
         }
     }
-    println!("{}", input.len())
 }
